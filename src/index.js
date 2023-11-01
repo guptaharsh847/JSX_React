0196ce2513ef6e0b40b61bfbba738f5d5bcfeed6
Harsh Gupta <guptaharsh847@gmail.com>
2023-11-01 19:58:28 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Favorite Beverages!</h1>
+    <ul>
+      <li>Coffee</li>
+      <li>Tea</li>
+      <li>Milk</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
